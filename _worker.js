9ad2beed20e6d0aa8f30c2a963bdd363358e5189
hyh1225w <115881944+hyh1225w@users.noreply.github.com>
2023-06-03 00:27:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "google.com";
+    url.host = "youtube.com";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
